41d053fd9f5a6e092438b4a54cfccba991829a26
sus <mnhat.nguyen.wk@gmail.com>
2026-06-04 20:44:42 +0900

    fix init

--- straggler/__init__.py
+++ straggler/__init__.py
@@ -1,6 +1,7 @@
-from .detector  import StraglerDetector
-from .gscm      import GSCM
-from .evaluator import DetectionEvaluator
+from straggler.detector  import StraglerDetector
+from straggler.gscm      import GSCM
+from straggler.evaluator import DetectionEvaluator
+from straggler.simulator import StragglerSimulator
 from .simulator import StragglerSimulator
 
 __all__ = ['StraglerDetector', 'GSCM', 'DetectionEvaluator', 'StragglerSimulator']
